c93774e1af397c8f41021ffbb2c2b21c2bc22130
Amer <amer.kawar@tweepi.com>
2019-08-23 18:16:18 -0700

    v.1.1.1

--- powerloop/grid_points.py
+++ powerloop/grid_points.py
@@ -127,10 +127,10 @@ class GridPoints():
             top_right = {'x': x + cover[0], 'y': y + cover[1]}
             points_in_sq = self._find_points_in_square(points, bottom_left, top_right)
             if len(points_in_sq) > 0:
-                avg_x = sum([points_in_sq[i]['x'] for i in range(0, len(points_in_sq))]) / len(points_in_sq)
-                avg_y = sum([points_in_sq[i]['y'] for i in range(0, len(points_in_sq))]) / len(points_in_sq)
+                # avg_x = sum([points_in_sq[i]['x'] for i in range(0, len(points_in_sq))]) / len(points_in_sq)
+                # avg_y = sum([points_in_sq[i]['y'] for i in range(0, len(points_in_sq))]) / len(points_in_sq)
                 # print("--> avg {}, {} for point {}, {}".format(avg_x, avg_y, x, y))
-                points_counts.append({'x': avg_x, 'y': avg_y, 'points': points_in_sq})
+                points_counts.append({'x': x, 'y': y, 'points': points_in_sq})
         # log('points_counts: {}'.format(len(points_counts)), title='load_points')
 
         square_with_max_plants = max(points_counts, key=lambda i: len(i['points']))
